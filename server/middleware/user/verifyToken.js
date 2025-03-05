a02e99a674bfdc96de12016ce0f92efb99323ed6
gulatipalak <gulatipalak2397@gmail.com>
2025-03-05 17:06:56 +0530

    removed extra console from verify Token middleware

--- server/middleware/user/verifyToken.js
+++ server/middleware/user/verifyToken.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
     const token = req.header("Authorization").split(" ")[1];
-    console.log(token);
 
     if(!token) return res.status(401).json({status: false, code: 401, message: "Access Denied! No token provided."});
 
