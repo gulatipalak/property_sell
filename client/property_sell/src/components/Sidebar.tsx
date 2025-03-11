7bf7c747c32fb83a69da963e0385e4ca79baebca
gulatipalak <gulatipalak2397@gmail.com>
2025-03-11 15:27:16 +0530

    sidebar menu mapping

--- client/property_sell/src/components/Sidebar.tsx
+++ client/property_sell/src/components/Sidebar.tsx
@@ -2,19 +2,17 @@ import { Link, useLocation} from "react-router-dom";
 
 const Sidebar = () => {
     const location = useLocation();
-  
+    const menuItems = ["dashboard","properties","settings"]
     return (
       <div className="w-64 min-h-screen bg-blue-800 text-white flex flex-col">
       <div className="p-6 text-[24px] font-bold">Property Bazaar</div>
       <nav className="sidebar-menu flex-1">
         <ul>
-          <li className={`px-[20px] py-[10px] hover:bg-blue-900 transition-all duration-300 ${location.pathname === "/dashboard" ? "active" : ""}`}>
-            <Link to="/dashboard" className="block">Dashboard</Link>
+          {menuItems.map((item,index) => (
+            <li key={index} className={`px-[20px] py-[10px] hover:bg-blue-900 transition-all duration-300 ${location.pathname === `${item}` ? "active" : ""}`}>
+            <Link to={`/${item}`} className="block capitalize">{item}</Link>
           </li>
-          <li className={`px-[20px] py-[10px] hover:bg-blue-900 transition-all duration-300 ${location.pathname === "/properties" ? "active" : ""}`}>
-            <Link to="/properties"
-            className="block">Properties</Link>
-          </li>
+           ))}
         </ul>
       </nav>
     </div>
